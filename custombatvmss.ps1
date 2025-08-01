7707b101bcfe626d679a42aa0c42a480af40016e
Gabriel cuadros <gabrielcuadros@gmail.com>
2025-07-31 20:34:24 -0500

    Update custombatvmss.ps1

--- custombatvmss.ps1
+++ custombatvmss.ps1
@@ -1,3 +1,3 @@
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\services\Tcpip\Parameters" /v "NV Domain" /t REG_SZ /d "eastus2.cloudapp.azure.com" /f
-winget install jqlang.jq
+winget.exe install jqlang.jq
