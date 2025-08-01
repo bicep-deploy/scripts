769cb69440745743f088fb7bbbdb8ad36a011b31
Gabriel cuadros <gabrielcuadros@gmail.com>
2025-08-01 01:46:40 -0500

    Update custombatvmss.ps1

--- custombatvmss.ps1
+++ custombatvmss.ps1
@@ -1,10 +1,11 @@
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
 Invoke-WebRequest https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-windows-amd64.exe -outfile C:\Windows\jq.exe -UseBasicParsing
 $response = Invoke-WebRequest -Uri "http://169.254.169.254/metadata/instance/compute?api-version=2021-02-01&format=json" -Headers @{ "Metadata" = "true" } -Method GET
-$name=$response.Content | jq '.name'
-$computername=$response.Content | jq '.vmScaleSetName' -r 
-$id=$name.split("_")[1].split('"')[0]
-$newname="vm$id"
+$response.Content | Out-File -FilePath "C:\metadata.json"
+$name = $response.Content | jq '.name'
+$computername = $response.Content | jq '.vmScaleSetName' -r 
+$id = $name.split("_")[1].split('"')[0]
+$newname = "vm$id"
 Rename-Computer -NewName $newname -Restart
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\services\Tcpip\Parameters" /v "NV Domain" /t REG_SZ /d "$computername.eastus2.cloudapp.azure.com" /f
 
