67035b86923b664745c12b8148995c3563b06017
Gabriel cuadros <gabrielcuadros@gmail.com>
2025-08-01 00:42:22 -0500

    Update custombatvmss.ps1

--- custombatvmss.ps1
+++ custombatvmss.ps1
@@ -1,8 +1,3 @@
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\services\Tcpip\Parameters" /v "NV Domain" /t REG_SZ /d "eastus2.cloudapp.azure.com" /f
-Install-PackageProvider -Name NuGet -Force 
-Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery
-Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
-Repair-WinGetPackageManager -AllUsers
-Write-Host "Done."
-winget.exe install jqlang.jq
+Invoke-WebRequest https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-windows-amd64.exe -outfile C:\Windows\jq.exe -UseBasicParsing
