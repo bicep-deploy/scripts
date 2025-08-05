f15c380a1e805c8a484f5ee42e2bf55532d96d89
Gabriel cuadros <gabrielcuadros@gmail.com>
2025-08-05 16:59:48 -0500

    Update customvm.ps1

--- customvm.ps1
+++ customvm.ps1
@@ -2,4 +2,3 @@ reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\R
 $response = Invoke-WebRequest -Uri "http://169.254.169.254/metadata/instance/compute?api-version=2021-02-01&format=json" -Headers @{ "Metadata" = "true" } -Method GET -outfile C:\prefile.json -UseBasicParsing
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\services\Tcpip\Parameters" /v "NV Domain" /t REG_SZ /d "eastus2.cloudapp.azure.com" /f
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
-Restart-Computer
