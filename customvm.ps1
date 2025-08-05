011c5c0955a82d14f114075a6d18cf8415ca08ab
Gabriel cuadros <gabrielcuadros@gmail.com>
2025-08-05 18:21:31 -0500

    Update customvm.ps1

--- customvm.ps1
+++ customvm.ps1
@@ -2,3 +2,10 @@ reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\R
 $response = Invoke-WebRequest -Uri "http://169.254.169.254/metadata/instance/compute?api-version=2021-02-01&format=json" -Headers @{ "Metadata" = "true" } -Method GET -outfile C:\prefile.json -UseBasicParsing
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\services\Tcpip\Parameters" /v "NV Domain" /t REG_SZ /d "eastus2.cloudapp.azure.com" /f
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
+$adapter = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1
+# Disable and enable the adapter
+Disable-NetAdapter -Name $adapter.Name -Confirm:$false
+Start-Sleep -Seconds 2
+Enable-NetAdapter -Name $adapter.Name -Confirm:$false
+netsh int ip reset
+ipconfig /registerdns
