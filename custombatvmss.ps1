92e63fc0f456d31f1e015a2848462a4446654072
Gabriel cuadros <gabrielcuadros@gmail.com>
2025-08-08 22:23:43 -0500

    Update custombatvmss.ps1

--- custombatvmss.ps1
+++ custombatvmss.ps1
@@ -9,4 +9,11 @@ Rename-Computer -NewName $newname -Restart
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"  -Name "NV Domain" -Value "$computername.eastus2.cloudapp.azure.com"  -Type String
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"  -Name 'Domain' -Value '$computername.eastus2.cloudapp.azure.com'  -Type String
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp"  -Name "UserAuthentication"  -Value 0  -Type DWord
-
+$adapter = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1
+# Disable and enable the adapter
+Disable-NetAdapter -Name $adapter.Name -Confirm:$false
+Start-Sleep -Seconds 2
+Enable-NetAdapter -Name $adapter.Name -Confirm:$false
+netsh int ip reset
+ipconfig /registerdns
+Restart-Computer -Force
