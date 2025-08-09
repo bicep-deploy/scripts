74845a1bb8856df73ad7d3bc01a9a8414ee594db
Gabriel cuadros <gabrielcuadros@gmail.com>
2025-08-09 01:11:51 -0500

    Update custombatvmss.ps1

--- custombatvmss.ps1
+++ custombatvmss.ps1
@@ -11,8 +11,3 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp"  -Name "UserAuthentication"  -Value 0  -Type DWord
 $adapter = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1
 # Disable and enable the adapter
-Disable-NetAdapter -Name $adapter.Name -Confirm:$false
-Start-Sleep -Seconds 2
-Enable-NetAdapter -Name $adapter.Name -Confirm:$false
-netsh int ip reset
-ipconfig /registerdns
